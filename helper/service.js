d70d3ada2a7242d4b9d3de590d229edbfb23b027
Yohann THEPAUT <contact@ythepaut.com>
2020-12-20 18:21:37 +0100

    Vues

--- helper/service.js
+++ helper/service.js
@@ -15,10 +15,23 @@ module.exports = class {
         // setting static folder
         this._app.use(this._express.static(this._path.join(__dirname, "../static")));
 
-        // home route
-        this._app.get("/", (req, res) => {
-            res.render("index");
-        });
+
+        // "GET" pages
+
+        let routeToRenderPages = {
+            "/" : "exercise/practice",
+            "/manage" : "exercise/list",
+            "/manage/new" : "exercise/new",
+            "/manage/edit/:id" : "exercise/edit",
+            "/about" : "about/about",
+            "/legal" : "about/legal"
+        }
+
+        for (let [route, renderPage] of Object.entries(routeToRenderPages)) {
+            this._app.get(route, (req, res) => {
+                res.render(renderPage);
+            })
+        }
 
     }
 }
