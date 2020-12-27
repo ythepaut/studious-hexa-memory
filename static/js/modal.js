77e6091b317165e21b7b59d90168dd545b16cbf8
Yohann THEPAUT <contact@ythepaut.com>
2020-12-27 20:16:10 +0100

    Ajout modal confirmation suppression

---
+++ static/js/modal.js
@@ -0,0 +1,15 @@
+document.querySelectorAll("[data-toggle=modal]").forEach((toggleElement) => {
+    console.log(toggleElement)
+    toggleElement.addEventListener("click", (event) => {
+        event.preventDefault();
+        let modal = document.querySelector("#" + toggleElement.getAttribute("data-target"));
+        modal.classList.add("is-active");
+    });
+});
+document.querySelectorAll(".modal-close, div.modal-background").forEach((toggleElement) => {
+    toggleElement.addEventListener("click", (event) => {
+        event.preventDefault();
+        let modal = document.querySelector("#" + toggleElement.getAttribute("data-target"));
+        modal.classList.remove("is-active");
+    });
+});
