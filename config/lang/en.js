5ee6b59c302b942ece2aac2a977a905e2e1391ec
Yohann THEPAUT <contact@ythepaut.com>
2021-04-04 18:36:48 +0200

    Traduction pages : A propos et mentions légales

--- config/lang/en.js
+++ config/lang/en.js
@@ -22,5 +22,22 @@ module.exports = {
 
     error: {
         generic: "<b>Oops</b>, an error has occured."
+    },
+
+    about: {
+        paragraph: "<b>Studious Hexa Memory</b> is a web application whose purpose is to help practicing exercises.\n" +
+            "            <br /><br />\n" +
+            "            The goal is not to respond to exercises from the site in the form of a MCQ, but rather to make the statement proposed by the exercise within a given time.<br />\n" +
+            "            Once this time has elapsed or after completing the exercise, you will be able to see its correction and validate or not your result.\n" +
+            "            <br /><br />\n" +
+            "            The features of Studious Hexa Memory are developed by <a href=\"https://www.ythepaut.com/\" target=\"_blank\">Yohann THEPAUT</a> and overseen <a href=\"https://www.youtube.com/channel/UCtgWzEBDJy_Jj7J7v2Y39UA\" target=\"_blank\">Baptiste BRUGEROLLE</a>.\n" +
+            "            <br /><br />\n" +
+            "            Source code available <a href=\"https://github.com/ythepaut/studious-hexa-memory\" target=\"_blank\">GitHub</a> under the GNU GPL v3 license."
+    },
+
+    legal: {
+        editor: "Editor :",
+        host: "Host",
+        hostedBy: "The website « ythepaut.com » is hosted by the OVHcloud company."
     }
 };
--- config/lang/fr.js
+++ config/lang/fr.js
@@ -22,5 +22,22 @@ module.exports = {
 
     error: {
         generic: "<b>Oups</b>, une erreur est survenue."
+    },
+
+    about: {
+        paragraph: "<b>Studious Hexa Memory</b> est un site d’entraînement et de révision sur des exercices proposés par ses utilisateurs.\n" +
+            "            <br /><br />\n" +
+            "            Le but n’est pas de répondre aux exercices depuis le site sous la forme d’un QCM, mais plutôt de faire l’énoncé proposé par l’exercice dans un temps imparti.<br />\n" +
+            "            Une fois ce temps écoulé ou après avoir fini l’exercice, vous pourrez consulter sa correction et valider ou non votre résultat.\n" +
+            "            <br /><br />\n" +
+            "            Les fonctionnalités de Studious Hexa Memory sont développées par <a href=\"https://www.ythepaut.com/\" target=\"_blank\">Yohann THEPAUT</a> et supervisées par <a href=\"https://www.youtube.com/channel/UCtgWzEBDJy_Jj7J7v2Y39UA\" target=\"_blank\">Baptiste BRUGEROLLE</a>.\n" +
+            "            <br /><br />\n" +
+            "            Code source disponible sur <a href=\"https://github.com/ythepaut/studious-hexa-memory\" target=\"_blank\">GitHub</a> sous licence GNU GPL v3."
+    },
+
+    legal: {
+        editor: "Editeur du site internet « ythepaut.com » :",
+        host: "Hebergeur",
+        hostedBy: "Le site « ythepaut.com » est hébergé par la société OVHcloud."
     }
 };
